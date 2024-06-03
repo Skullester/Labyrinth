69e9c98760f73db1c07d5c07b8875b8043582f5b
Alexander <138435791+Skullester@users.noreply.github.com>
2024-06-04 01:38:04 +0400

    commit

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80f1a676594bfb33acf61a0ed0d5c98858354fd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76b085a17c6ba3e502c0ceac833bc07ec71ca7cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
